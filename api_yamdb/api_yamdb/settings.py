3eee382a27ec49954330c8ac6356cb110bc1f97a
Катрин Сахарова <jessup@yandex.ru>
2023-04-18 01:31:06 +0300

    Add deploy

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '51.250.72.161', ]
 
 INSTALLED_APPS = [
     'drf_spectacular',
